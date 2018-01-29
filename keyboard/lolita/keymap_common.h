786bba476db9f7b0cce7ef2f408ad31d199092dd
dugo <35316009+danfkt@users.noreply.github.com>
2018-01-29 13:05:17 +0100

    Update keymap_common.h

--- keyboard/lolita/keymap_common.h
+++ keyboard/lolita/keymap_common.h
@@ -41,36 +41,34 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* TKL ANSI keymap definition macro */
 #define KEYMAP( \
-    K00,      K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D,    K0E, K0F, K0G,   \
-    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D,    K1E, K1F, K1G,   \
-    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D,    K2E, K2F, K2G,   \
-    K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B,      K3D,                     \
-    K40, K41, K42, K43, K44, K45, K46, K47, K48, K49, K4A,           K4D,         K4F,        \
-    K50, K51, K52,           K55,                K59, K5A,      K5C, K5D,    K5E, K5F, K5G    \
+    K000,       K002, K003, K004, K005, K006, K007, K008, K009, K010, K011, K012, K013,         K014, K015, K016,   \
+    K100, K101, K102, K103, K104, K105, K106, K107, K108, K109, K110, K111, K112, K113,         K114, K115, K116,   \
+    K200, K201, K202, K203, K204, K205, K206, K207, K208, K209, K210, K211, K212, K213,         K214, K215, K216,   \
+    K300, K301, K302, K303, K304, K305, K306, K307, K308, K309, K310, K311,       K313,                             \
+    K400,       K402, K403, K404, K405, K406, K407, K408, K409, K410, K411,       K413,               K415,         \
+    K500, K501, K502,                   K506,                   K510, K511, K512, K513,         K514, K515, K516    \
 ) { \
-    { KC_##K00, KC_NO,    KC_##K02, KC_##K03, KC_##K04, KC_##K05, KC_##K06, KC_##K07, KC_##K08, KC_##K09, KC_##K0A, KC_##K0B, KC_##K0C, KC_##K0D,    KC_##K0E, KC_##K0F, KC_##K0G }, \
-    { KC_##K10, KC_##K11, KC_##K12, KC_##K13, KC_##K14, KC_##K15, KC_##K16, KC_##K17, KC_##K18, KC_##K19, KC_##K1A, KC_##K1B, KC_##K1C, KC_##K1D,    KC_##K1E, KC_##K1F, KC_##K1G }, \
-    { KC_##K20, KC_##K21, KC_##K22, KC_##K23, KC_##K24, KC_##K25, KC_##K26, KC_##K27, KC_##K28, KC_##K29, KC_##K2A, KC_##K2B, KC_##K2C, KC_##K2D,    KC_##K2E, KC_##K2F, KC_##K2G }, \
-    { KC_##K30, KC_##K31, KC_##K32, KC_##K33, KC_##K34, KC_##K35, KC_##K36, KC_##K37, KC_##K38, KC_##K39, KC_##K3A, KC_##K3B, KC_NO,    KC_##K3D,    KC_NO,    KC_NO,    KC_NO    }, \
-    { KC_##K40, KC_##K41, KC_##K42, KC_##K43, KC_##K44, KC_##K45, KC_##K46, KC_##K47, KC_##K48, KC_##K49, KC_##K4A, KC_NO,    KC_NO,    KC_##K4D,    KC_NO,    KC_##K4F, KC_NO    }, \
-    { KC_##K50, KC_##K51, KC_##K52, KC_NO,    KC_NO,    KC_##K55, KC_NO,    KC_NO,    KC_NO,    KC_##K59, KC_##K5A, KC_NO,    KC_##K5C, KC_##K5D,    KC_##K5E, KC_##K5F, KC_##K5G }  \
+    { KC_##K000, KC_NO,     KC_##K002, KC_##K003, KC_##K004, KC_##K005, KC_##K006, KC_##K007, KC_##K008, KC_##K009, KC_##K010, KC_##K011, KC_##K012, KC_##K013,        KC_##K014, KC_##K015, KC_##K016 }, \
+    { KC_##K100, KC_##K101, KC_##K102, KC_##K103, KC_##K104, KC_##K105, KC_##K106, KC_##K107, KC_##K108, KC_##K109, KC_##K110, KC_##K111, KC_##K112, KC_##K113,        KC_##K114, KC_##K115, KC_##K116 }, \
+    { KC_##K200, KC_##K201, KC_##K202, KC_##K203, KC_##K204, KC_##K205, KC_##K206, KC_##K207, KC_##K208, KC_##K209, KC_##K210, KC_##K211, KC_##K212, KC_##K213,        KC_##K214, KC_##K215, KC_##K216 }, \
+    { KC_##K300, KC_##K301, KC_##K302, KC_##K303, KC_##K304, KC_##K305, KC_##K306, KC_##K307, KC_##K308, KC_##K309, KC_##K310, KC_##K311, KC_NO,     KC_##K313,        KC_NO,     KC_NO,     KC_NO     }, \
+    { KC_##K400, KC_NO,     KC_##K402, KC_##K403, KC_##K404, KC_##K405, KC_##K406, KC_##K407, KC_##K408, KC_##K409, KC_##K410, KC_##K411, KC_NO,     KC_##K413,        KC_NO,     KC_##K415, KC_NO     }, \
+    { KC_##K500, KC_##K501, KC_##K502, KC_NO,     KC_NO,     KC_NO,     KC_##K506, KC_NO,     KC_NO,     KC_NO,     KC_##K510, KC_##K511, KC_##K512, KC_##K513,        KC_##K514, KC_##K515, KC_##K516 }  \
 }
 
-
 #define KEYMAP_LOLITA( \
-    K00,      K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D,    K0E, K0F, K0G,   \
-    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D,    K1E, K1F, K1G,   \
-    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D,    K2E, K2F, K2G,   \
-    K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B,      K3D,                     \
-    K40, K41, K42, K43, K44, K45, K46, K47, K48, K49, K4A,           K4D,         K4F,        \
-    K50, K51, K52,           K55,                K59, K5A,      K5C, K5D,    K5E, K5F, K5G    \
+    K000,       K002, K003, K004, K005, K006, K007, K008, K009, K010, K011, K012, K013,    K014, K015, K016,   \
+    K100, K101, K102, K103, K104, K105, K106, K107, K108, K109, K110, K111, K112, K113,    K114, K115, K116,   \
+    K200, K201, K202, K203, K204, K205, K206, K207, K208, K209, K210, K211, K212, K213,    K214, K215, K216,   \
+    K300, K301, K302, K303, K304, K305, K306, K307, K308, K309, K310, K311,       K313,                        \
+    K400,       K402, K403, K404, K405, K406, K407, K408, K409, K410, K411,       K413,          K415,         \
+    K500, K501, K502,                   K506,                   K510, K511, K512, K513,    K514, K515G,K516    \
 ) KEYMAP( \
-    K00,      K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D,    K0E, K0F, K0G,   \
-    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D,    K1E, K1F, K1G,   \
-    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D,    K2E, K2F, K2G,   \
-    K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, NO,  K3D,    NO,  NO,  NO,    \
-    K40, K41, K42, K43, K44, K45, K46, K47, K48, K49, K4A,      NO,  K4D,    NO,  K4F, NO,    \
-    K50, K51, K52,           K55,                K59, K5A,      K5C, K5D,    K5E, K5F, K5G    \
+    K000, NO,   K002, K003, K004, K005, K006, K007, K008, K009, K010, K011, K012, K013,    K014, K015, K016,   \
+    K100, K101, K102, K103, K104, K105, K106, K107, K108, K109, K110, K111, K112, K113,    K114, K115, K116,   \
+    K200, K201, K202, K203, K204, K205, K206, K207, K208, K209, K210, K211, K212, K213,    K214, K215, K216,   \
+    K300, K301, K302, K303, K304, K305, K306, K307, K308, K309, K310, K311, NO,   K313,    NO,   NO,   NO,     \
+    K400, NO,   K402, K403, K404, K405, K406, K407, K408, K409, K410, K411, NO,   K413,    NO,   K415, NO,    \
+    K500, K501, K502, NO,   NO,   NO,   K506, NO,   NO,   NO,   K510, K511, K512, K513,    K514, K515G,K516    \
 )
-
 #endif
